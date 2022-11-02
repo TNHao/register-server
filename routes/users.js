7cf740a98fa35d046eca92103ea64313540c1643
haotn <haotn@vng.com.vn>
2022-11-02 13:10:21 +0700

    update server

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,51 @@
 var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
 
-/* GET users listing. */
-router.post("/", function (req, res, next) {
+const saltRounds = 10;
+const userList = [];
+
+router.post("/register", function (req, res, next) {
   const { body } = req;
+
+  const userInfo = userList.find((user) => user.email === body.email);
+
+  if (userInfo) {
+    res.send({ error: "Tài khoản đã tồn tại!!!" });
+    return;
+  }
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(body.password, salt);
+
+  const { confirmPassword, password, ...rest } = body;
+
+  userList.push({ ...rest, password: hash });
   res.send(body);
+});
+
+router.post("/login", function (req, res, next) {
+  const { body } = req;
+
+  const userInfo = userList.find((user) => user.email === body.email);
+
+  if (!userInfo) {
+    res.send({ error: "Tài khoản không tồn tại!!!" });
+    return;
+  }
+
+  const check = bcrypt.compareSync(body.password, userInfo.password);
+
+  if (check) {
+    res.send(userInfo);
+  } else res.send({ error: "Sai mật khẩu!!!" });
+});
+
+router.get("/", function (req, res, next) {
+  const { email } = req.query;
+  const user = userList.find((user) => user.email === email);
+
+  res.send(user ? user : { error: "Không tìm thấy" });
 });
 
 module.exports = router;
